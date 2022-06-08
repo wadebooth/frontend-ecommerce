c9448062abe2ce670e060de55ddac6246036d87e
wadebooth <wademgmt@gmail.com>
2022-06-08 15:06:34 -0400

    fetched products from api using axios

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,15 +2,20 @@ import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import products from '../products'
+import axios from 'axios'
 
 const HomeScreen = () => {
   const [products, setProducts] = useState([])
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const res = await axios.get('/api/products')
+      const { data } = await axios.get('/api/products')
+
+      setProducts(data)
     }
-  })
+
+    fetchProducts()
+  }, [])
 
   return (
     <>
