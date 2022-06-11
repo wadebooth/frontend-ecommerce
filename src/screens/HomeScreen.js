0434092cc468a2697bde04537e545acf9a9131d7
wadebooth <wademgmt@gmail.com>
2022-06-11 16:22:57 -0400

    commented out product map issue

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -21,13 +21,13 @@ const HomeScreen = () => {
     <>
       <h1>Latest Products</h1>
       <Row>
-        {products.map((product) => {
+        {/* {products.map((product) => {
           return (
             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
               <Product product={product} />
             </Col>
           )
-        })}
+        })} */}
       </Row>
     </>
   )
