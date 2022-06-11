66a52fb43c6bd5dc1de464d3d9f68e41869f8ddf
wadebooth <wademgmt@gmail.com>
2022-06-11 16:15:27 -0400

    adjusted port

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -21,7 +21,7 @@ const HomeScreen = () => {
     <>
       <h1>Latest Products</h1>
       <Row>
-        {products?.map((product) => {
+        {products.map((product) => {
           return (
             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
               <Product product={product} />
