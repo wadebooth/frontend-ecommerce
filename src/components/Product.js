7250da11698f386c5589a4f780d8552ecc8108f9
wadebooth <wademgmt@gmail.com>
2022-06-12 18:39:27 -0400

    commented out product map issue

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom'
 import { Card } from 'react-bootstrap'
 import Rating from './Rating'
 
+//creating a product
+
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
       <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top' />
-        </Link>
+      </Link>
 
       <Card.Body>
         <Link to={`/product/${product._id}`}>
