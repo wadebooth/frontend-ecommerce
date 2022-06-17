a6fbf8d1ce1328a85b864b31e242273480138cc7
wadebooth <wademgmt@gmail.com>
2022-06-16 21:18:45 -0400

    added login styling

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,7 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import FormContainer from '../components/FormContainer'
 
-const LoginScreen = () => {
-  return <div>Login</div>
+const LoginScreen = ({ location }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  //   const redirect = location.search ? location.search.split('=')[1] : '/'
+
+  const sumbitHandler = (e) => {
+    e.preventDefault()
+    // DISPATCH LOGIN
+  }
+
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+      <Form onSubmit={sumbitHandler}>
+        <Form.Group controlId='email'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+
+          <Form.Group controlId='password'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Enter Password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+        </Form.Group>
+
+        <Button type='Submit' variant='primary'>
+          Sign In
+        </Button>
+      </Form>
+
+      <Row className='py-3'>
+        <Col>
+          New Customer?{' '}
+          {/* <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+            Register
+          </Link> */}
+        </Col>
+      </Row>
+    </FormContainer>
+  )
 }
 
 export default LoginScreen
