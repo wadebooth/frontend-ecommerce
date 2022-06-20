5d56460671de83716247231899c450b89f2f02a9
wadebooth <wademgmt@gmail.com>
2022-06-20 16:34:11 -0400

    returned stash

---
+++ src/components/GoogleSignUp.js
@@ -0,0 +1,16 @@
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
+
+const GoogleSignUp = getAuth()
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code
+    const errorMessage = error.message
+    // ..
+  })
+
+export default GoogleSignUp
