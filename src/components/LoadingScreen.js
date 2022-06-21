911f21b34d91817187d6cfe1ac87ab0a88854997
wadebooth <wademgmt@gmail.com>
2022-06-21 00:29:23 -0400

    moved loading bar to the middle

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -3,7 +3,14 @@ import ReactLoading from 'react-loading'
 
 export default function LoadingScreen() {
   return (
-    <div>
+    <div
+      style={{
+        // do your styles depending on your needs.
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
       <ReactLoading type='balls' color='#0000FF' height={100} width={50} />
     </div>
   )
