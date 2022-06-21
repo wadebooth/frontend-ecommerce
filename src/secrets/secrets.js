1342a82203c0365784102b6a82722fae758f2f61
wadebooth <wademgmt@gmail.com>
2022-06-20 23:55:38 -0400

    setup loading feature

---
+++ src/secrets/secrets.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBhh2TxeDYt7vmgVFg3SOsyNth65WhvLlg',
+  authDomain: 'ecommerce-frontend-wb.firebaseapp.com',
+  projectId: 'ecommerce-frontend-wb',
+  storageBucket: 'ecommerce-frontend-wb.appspot.com',
+  messagingSenderId: '172124178536',
+  appId: '1:172124178536:web:671c5f7d97cd45f6cf93dd',
+}
+
+export default initializeApp(firebaseConfig)
